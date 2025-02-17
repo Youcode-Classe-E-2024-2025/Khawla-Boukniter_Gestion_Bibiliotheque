04211dae1316da7ab102aaff00ed1ef5fe4712e4
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-02-17 14:45:00 +0100

    Create basic models: Book, User, Borrowing

---
+++ app/Models/Borrowing.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Borrowing extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'book_id',
+        'borrowed_at',
+        'returned_at',
+    ];
+
+    protected $casts = [
+        'borrowed_at' => 'datetime',
+        'returned_at' => 'datetime'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
+
+    public function isOverdue()
+    {
+        return !$this->returned_at && $this->borrowed_at->addDays(14)->isPast();
+    }
+}
